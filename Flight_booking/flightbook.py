b055def0118747ad4a54e4811dbca84acf825c56
ttr-rtt <ttrbbkry@gmail.com>
2024-10-16 15:25:39 -0400

    Feature_BookingForm

--- Flight_booking/flightbook.py
+++ Flight_booking/flightbook.py
@@ -9,10 +9,10 @@ password_list = ["aaa", "123"]
 email_list = ["aaa@gmail.com", "123@gmail.com"]
 
 
-@app.route('/')
-def index():
-    error = session.pop('error', None)
-    return render_template('login.html', error=error)
+# @app.route('/')
+# def index():
+#     error = session.pop('error', None)
+#     return render_template('login.html', error=error)
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -65,5 +65,74 @@ def register():
 def login_1():
     return render_template('login.html')
 
+
+# Mock backend storage, a list to store passager data
+passengers_data_store = []
+
+@app.route('/')
+def home():
+    return redirect(url_for('passenger_info'))
+
+@app.route('/passenger_info', methods=['GET', 'POST'])
+def passenger_info():
+    if request.method == 'POST':
+        # Store passenger data in a dictionary
+        passenger_data = {
+            'number_of_passengers': request.form['passengers'],
+            'first_name_1': request.form['first_name_1'],
+            'last_name_1': request.form['last_name_1'],
+            'middle_name_1': request.form.get('middle_name_1', ''),
+            'id_number_1': request.form.get('id_number_1', ''),
+            'email_1': request.form['email_1'],
+            'phone_1': request.form['phone_1'],
+            'dob_1': request.form['dob_1'],
+            'address1_1': request.form['address1_1'],
+            'address2_1': request.form.get('address2_1', ''),
+            'country_1': request.form['country_1'],
+            'city_1': request.form['city_1'],
+            'postal_code_1': request.form['postal_code_1'],
+            'em_first_name': request.form['em_first_name'],
+            'em_last_name': request.form['em_last_name'],
+            'em_phone': request.form['em_phone'],
+            'em_email': request.form['em_email'],
+            'bags': request.form['bags']
+        }
+
+        # If there is a second passenger, add their details too
+        if request.form['passengers'] == "2":
+            passenger_data.update({
+                'first_name_2': request.form['first_name_2'],
+                'last_name_2': request.form['last_name_2'],
+                'middle_name_2': request.form.get('middle_name_2', ''),
+                'id_number_2': request.form.get('id_number_2', ''),
+                'dob_2': request.form['dob_2'],
+                'email_2': request.form.get('email_2', ''),
+                'phone_2': request.form.get('phone_2', ''),
+                'address1_2': request.form.get('address1_2', ''),
+                'address2_2': request.form.get('address2_2', ''),
+                'country_2': request.form.get('country_2', ''),
+                'city_2': request.form.get('city_2', ''),
+                'postal_code_2': request.form.get('postal_code_2', '')
+            })
+            
+        # Append the passenger data to the list
+        passengers_data_store.append(passenger_data)
+
+        # Print passenger data
+        print("Stored Passenger Data:")
+        for passenger in passengers_data_store:
+            print(passenger)
+        # Save passenger data
+        return "Passenger information submitted!"
+
+    
+    return render_template('booking.html')
+
+@app.route('/view_passenger_data')
+def view_passenger_data():
+    # Return the entire list of stored passenger data
+    return {"passenger_data": passengers_data_store}
+
 if __name__ == '__main__':
     app.run(debug=True)
+    
