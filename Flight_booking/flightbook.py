73c84c4f6d23fb8734184dd32b4d0d003fe376c8
reimei128 <21yh48@queensu.ca>
2024-10-16 15:21:26 -0400

    homepage

--- Flight_booking/flightbook.py
+++ Flight_booking/flightbook.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 app.secret_key = 'aa2233'
 
@@ -11,15 +11,81 @@ email_list = ["aaa@gmail.com", "123@gmail.com"]
 
 @app.route('/')
 def index():
-    error = session.pop('error', None)
-    return render_template('login.html', error=error)
+    # session['current_username'] = 'aaa'
+    current_username = session.get('current_username', "Login / Sign up")
+    return render_template('homepage.html', current_username=current_username)
+
+
+@app.route('/process-booking', methods=['POST'])
+def process_booking():
+    # 获取用户提交的表单数据
+    from_city = request.form.get('from_city')
+    to_city = request.form.get('to_city')
+    travellers_class = request.form.get('travellers_class')
+    departure_date = request.form.get('departure_date')
+    return_date = request.form.get('return_date')
+
+    # 你可以在这里处理数据，例如打印到控制台或保存到数据库
+    print(f"From: {from_city}")
+    print(f"To: {to_city}")
+    print(f"Travellers and Class: {travellers_class}")
+    print(f"Departure Date: {departure_date}")
+    print(f"Return Date: {return_date}")
+
+    # 返回一个响应给用户
+    return f"Booking details: From {from_city} to {to_city} for {travellers_class}, departing on {departure_date}, returning on {return_date}"
+
+
+@app.route('/flights')
+def flights():
+    return render_template('homepage.html')
+
+
+@app.route('/booking-history')
+def booking_history():
+    return 'booking_history.html'
+
+
+@app.route('/login_page')
+def login_page():
+    return 'login.html'
+
+
+@app.route('/cart')
+def cart():
+    return 'cart.html'
+
+
+@app.route('/logout')
+def logout():
+    session.pop('current_username', None)
+    return redirect(url_for('index'))
+
+
+
+@app.route('/search-results')
+def search_results():
+    # 获取查询参数
+    from_city = request.args.get('from')
+    to_city = request.args.get('to')
+    travellers = request.args.get('travellers')
+    departure = request.args.get('departure')
+    return_date = request.args.get('return')
+
+    # 处理搜索逻辑并返回结果页面
+    return 'searching result'
+
+#
+# @app.route('/')
+# def index():
+#     error = session.pop('error', None)
+#     return render_template('login.html', error=error)
 
 @app.route('/login', methods=['POST'])
 def login():
     username = request.form['username']
     password = request.form['password']
-
-
+    
     if username in username_list and password in password_list:
         user_index = username_list.index(username)
         if password_list[user_index] == password:
