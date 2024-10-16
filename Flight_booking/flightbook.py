aa2076451202d6dc83695c8e36653846636711d2
Etern11ty <zhongrenzhao@hotmail.com>
2024-10-16 14:19:56 -0400

    login process

--- Flight_booking/flightbook.py
+++ Flight_booking/flightbook.py
@@ -1,10 +1,69 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('login.html')  # 渲染 index.html 模板
+app.secret_key = 'aa2233'
 
-if __name__ == "__main__":
+username_list = ["aaa", "123"]
+password_list = ["aaa", "123"]
+email_list = ["aaa@gmail.com", "123@gmail.com"]
+
+
+@app.route('/')
+def index():
+    error = session.pop('error', None)
+    return render_template('login.html', error=error)
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+
+    if username in username_list and password in password_list:
+        user_index = username_list.index(username)
+        if password_list[user_index] == password:
+
+            session['current_username'] = username
+
+            return redirect(url_for('page_a'))
+        
+    session['error'] = "Invalid username or password"  
+    return redirect(url_for('index')) 
+
+@app.route('/page_a')
+def page_a():
+    return "Welcome to Page A!"
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        email = request.form['email']
+
+        if password != confirm_password:
+            return render_template('register.html', error="Passwords do not match")
+        elif username in username_list:
+            return render_template('register.html', error="Username is occupied")
+        elif email in email_list:
+            return render_template('register.html', error="Email is occupied")
+
+
+        username_list.append(username)
+        password_list.append(password)
+        email_list.append(email)
+
+        print(username_list, password_list, email_list)
+
+        return "homepage"
+
+    return render_template('register.html')
+
+@app.route('/login_1')
+def login_1():
+    return render_template('login.html')
+
+if __name__ == '__main__':
     app.run(debug=True)
