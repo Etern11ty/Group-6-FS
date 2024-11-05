aab75995b3652a1fb9d2c34fb5fb92b582ded07c
ZHAO Zhongren <zhongrenzhao@hotmail.com>
2024-11-05 00:14:12 -0500

    update port

--- Flight_booking/flightbook.py
+++ Flight_booking/flightbook.py
@@ -453,7 +453,6 @@ def seat_confirmation(flight_id, seat):
 
 
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
-    
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080)) 
+    app.run(host="0.0.0.0", port=port)
